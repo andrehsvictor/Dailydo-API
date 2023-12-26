03b7173b7d75977daf950d53da5ef41d19387a8d
André Victor <andrehsvictor@gmail.com>
2023-12-26 13:57:49 -0300

    feat: added a index endpoint

--- src/main/java/andrehsvictor/api/dailydo/DailydoApplication.java
+++ src/main/java/andrehsvictor/api/dailydo/DailydoApplication.java
@@ -2,9 +2,19 @@ package andrehsvictor.api.dailydo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+
+@RestController
 @SpringBootApplication
 public class DailydoApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "Welcome to Dailydo API!";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailydoApplication.class, args);
